ea1d47ca2c4d9d80a405a3cbe14f335d8a7689aa
Jonathan Pharies <pharies@Jonathans-MacBook-Pro.local>
2019-09-18 16:00:54 -0400

    button turning off and on

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 // java script for the blackjack game
 var dealNum = 0;
 var playNum = 0;
+var black = document.getElementById("label")
+var person = document.getElementById("player");
+ var dealer = document.getElementById("dealer");
 
-
+document.getElementById("pass").disabled = true;
+document.getElementById("hit").disabled = true;
 
 // sets up the game by giving the player and the dealer two random "cards" 
 var start = function()
 {
     
     // set up the dealer
-    var dealer = document.getElementById("dealer");
     var card1 = math();
     var card2 = math();
     dealNum = card1 + card2;
@@ -17,7 +20,6 @@ var start = function()
     dealer.innerHTML = strDealNum;
     
     // set up player
-    var person = document.getElementById("player");
     card1 = math();
     card2 = math();
     playNum = card1 + card2;
@@ -26,14 +28,37 @@ var start = function()
     
     
     // reset the board
-    var black = document.getElementById("label")
     black.innerHTML = "BlackJack";
     
-    // make sure they can click the button until after the game is played
-    document.getElementById("start").onclick = null;
+    // make sure they can click the button until after the game is played/ enable the other two buttons
+    document.getElementById("start").disabled = true;
+    document.getElementById("pass").disabled = false;
+    document.getElementById("hit").disabled = false;
     
 };
 
+var pass = function()
+{
+    
+    // make dealer go unti he >= 15
+        // while loop
+    
+    // compare the player and dealer num
+    
+    // project who wins in the title
+    
+    // make sure they can click the button until after the game is played / disable ability to play
+    document.getElementById("start").disabled = false;
+    document.getElementById("pass").disabled = true;
+    document.getElementById("hit").disabled = true;
+    
+    
+    
+    
+    
+    
+    
+};
 
 
 // randomly generates a number from 1-10
@@ -47,4 +72,4 @@ var math = function()
     return ran;
     
     
-}
+};
